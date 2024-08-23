e854cabfa058fd41d616a35adeb9664064e076fe
mrali@ns <angryredninja26@gmail.com>
2024-08-23 23:31:01 +0330

    Add spinlock

---
+++ spinlock/src/lib.rs
@@ -0,0 +1,47 @@
+use std::cell::UnsafeCell;
+use std::hint;
+use std::ops::{Deref, DerefMut};
+use std::sync::atomic::{AtomicBool, Ordering::Acquire, Ordering::Release};
+#[derive(Debug)]
+pub struct SpinLock<T> {
+    locked: AtomicBool,
+    value: UnsafeCell<T>,
+}
+
+pub struct Guard<'a, T> {
+    lock: &'a SpinLock<T>,
+}
+impl<T> SpinLock<T> {
+    pub const fn new(value: T) -> Self {
+        Self {
+            locked: AtomicBool::new(false),
+            value: UnsafeCell::new(value),
+        }
+    }
+    pub fn lock<'a>(&'a self) -> Guard<T> {
+        while self.locked.swap(true, Acquire) {
+            hint::spin_loop()
+        }
+        Guard { lock: self }
+    }
+    pub fn unlock(&self) {
+        self.locked.swap(false, Release);
+    }
+}
+impl<T> Deref for Guard<'_, T> {
+    type Target = T;
+    fn deref(&self) -> &Self::Target {
+        unsafe { &*self.lock.value.get() }
+    }
+}
+impl<T> DerefMut for Guard<'_, T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        unsafe { &mut *self.lock.value.get() }
+    }
+}
+impl<T> Drop for Guard<'_, T> {
+    fn drop(&mut self) {
+        self.lock.locked.swap(false, Release);
+    }
+}
+unsafe impl<T> Sync for SpinLock<T> where T: Send {}
